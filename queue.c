ab8d6f765b586b262a6478a1da226a81aa6744c0
eximus <fabio_r11@hotmail.com>
2015-11-16 21:25:06 +0000

    final corrections, after evaluation

---
+++ queue.c
@@ -0,0 +1,58 @@
+
+
+#include "queue.h"
+#include <stdlib.h>
+
+
+/* ----------------------------------------------------------
+ * Make new queue
+ * ---------------------------------------------------------- */
+queue_l *new_queue(){
+	queue_l *queue;
+	queue = (queue_l*)malloc(sizeof(queue_l));
+	queue->head = NULL;
+	queue->tail = NULL;
+	return queue;
+}
+
+/* ----------------------------------------------------------
+ * Add a new node to the queue with given pid
+ * Other fields Uninitialized
+ * ---------------------------------------------------------- */
+void enqueue(struct queue *queue_list, int pid){
+	if (queue_list == NULL)
+		return;
+	struct node *in_node = (struct node*)malloc(sizeof(struct node));
+	in_node->process_pid = pid;
+	in_node->next = NULL;
+
+	if (queue_list->head == NULL)
+		queue_list->head = in_node;
+	if (queue_list->tail == NULL)
+		queue_list->tail = in_node;
+	else{
+		queue_list->tail->next = in_node;
+		queue_list->tail = in_node;
+	}
+}
+
+/* ----------------------------------------------------------
+ * Remove element from list, must be freed
+ * ---------------------------------------------------------- */
+int dequeue(struct queue *queue_list){
+	int pid;
+	struct node *out_node = NULL;
+	if (queue_list == NULL || queue_list->head == NULL)
+		return -1;
+	out_node = queue_list->head;
+	if (queue_list->head->next != NULL){
+		queue_list->head = out_node->next;
+	}
+	else{ /* queue ended, no more nodes */
+		queue_list->head = NULL;
+		queue_list->tail = NULL;
+	}
+	pid = out_node->process_pid;
+	free(out_node);
+	return pid;
+}
